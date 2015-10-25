848727ba1322ff51fcbae248ca61c308dc62873b
Kurt Kanzenbach <kurt@kmk-computers.de>
2015-10-25 13:01:42 +0100

    added dir-locals for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;; Coding style
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 80)
+         (tab-width . 4)))
+ (c-mode . ((c-file-style . "linux")
+            (c-basic-offset . 4))))
