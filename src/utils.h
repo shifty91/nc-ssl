ec40b7c2a8aa9973509e2cb35d73ef5baabc410e
Kurt Kanzenbach <kurt@kmk-computers.de>
2016-05-16 18:26:17 +0200

    utils: shortening file names

--- src/utils.h
+++ src/utils.h
@@ -3,33 +3,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <libgen.h>
 
 #include "config.h"
 
 /* printing */
 #define err(fmt, ...)                                                   \
     do {                                                                \
-        fprintf(stderr, "[ERROR %s:%d]: " fmt "\n", __FILE__, __LINE__,  ##__VA_ARGS__); \
+        fprintf(stderr, "[ERROR %s:%d]: " fmt "\n", basename(__FILE__), \
+                __LINE__,  ##__VA_ARGS__);                              \
         exit(EXIT_FAILURE);                                             \
     } while (0)
 #define serr(str) err("%s", str)
 
 #define log_err(fmt, ...)                                               \
     do {                                                                \
-        fprintf(stderr, "[ERROR %s:%d]: " fmt "\n", __FILE__, __LINE__,  ##__VA_ARGS__); \
+        fprintf(stderr, "[ERROR %s:%d]: " fmt "\n", basename(__FILE__), \
+                __LINE__,  ##__VA_ARGS__);                              \
     } while (0)
 #define log_serr(str) log_err("%s", str)
 
 #define dbg(fmt, ...)                                                   \
     do {                                                                \
         if (config.debug)                                               \
-            printf("[DEBUG %s:%d]: " fmt "\n", __FILE__, __LINE__,  ##__VA_ARGS__); \
+            printf("[DEBUG %s:%d]: " fmt "\n", basename(__FILE__),      \
+                   __LINE__,  ##__VA_ARGS__);                           \
     } while (0)
 #define sdbg(str) dbg("%s", str)
 
 #define info(fmt, ...)                                                  \
     do {                                                                \
-        printf("[INFO %s:%d]: " fmt "\n", __FILE__, __LINE__,  ##__VA_ARGS__); \
+        printf("[INFO %s:%d]: " fmt "\n", basename(__FILE__), __LINE__, \
+               ##__VA_ARGS__);                                          \
     } while (0)
 #define sinfo(str) info("%s", str)
 
